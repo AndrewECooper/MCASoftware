90237b82f7db523a2bbe8751bf8152bbe7ff1324
Matthew Walters <magnissimumsum@gmail.com>
2018-05-10 18:37:41 -0400

    removing bad comments

--- bundles/ranvier-quests/quest-rewards/MoveReward.js
+++ bundles/ranvier-quests/quest-rewards/MoveReward.js
@@ -4,13 +4,7 @@ const LevelUtil = require('../../ranvier-lib/lib/LevelUtil');
 
 module.exports = srcPath => {
   const QuestReward = require(srcPath + 'QuestReward');
-  /**
-   * Quest reward that gives experience
-   *
-   * Config options:
-   *   currency: string, required, currency to award
-   *   amount: number, required
-   */
+
   return class MoveReward extends QuestReward {
     static reward(GameState, quest, config, player) {
       const mover = this._getStuff(quest, config);
