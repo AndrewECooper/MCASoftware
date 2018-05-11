b7ba7bc584991f5f2e1c96a4f940593b8c058ca8
Matthew Walters <magnissimumsum@gmail.com>
2018-05-11 14:21:52 -0400

    trying out stuff to move guy, didn't work

--- bundles/ranvier-quests/quest-rewards/MoveReward.js
+++ bundles/ranvier-quests/quest-rewards/MoveReward.js
@@ -7,23 +7,22 @@ module.exports = srcPath => {
 
   return class MoveReward extends QuestReward {
     static reward(GameState, quest, config, player) {
-
-    }
+      let npc;
+      state.MobManager.mobs.forEach(function(mob) {
+        if (mob.id == 6 && mob.area.name == "Prison") npc = mob;
+      });
   
+      let destination = state.RoomManager.rooms.get("Prison:11");
+  
+      npc.moveTo(destination, ()    => {
+        Broadcast.sayAt(player, "The creepy old dude runs away screaming!");
+      });
+      console.log("activation");
+    }
+      
+
     static display(GameState, quest, config, player) {
       return ``
-    }
-    
-
-    static _getStuff(quest, config) {
-      config = Object.assign({
-      }, config);
-
-      if (!config.mover) {
-        throw new Error(`Quest [${quest.id}] currency reward has invalid configuration`);
-      }
-
-      return config.mover;
     }
   };
 };
